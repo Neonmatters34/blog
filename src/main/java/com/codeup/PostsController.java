40d4086d4b7c34f5d3bddf056b360f39ef63c49c
Matthew Garza <matt.garza34@gmail.com>
2017-01-05 17:00:59 -0600

    create page up

--- src/main/java/com/codeup/PostsController.java
+++ src/main/java/com/codeup/PostsController.java
@@ -5,6 +5,8 @@ import com.codeup.models.Post;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
 
@@ -18,5 +20,15 @@ public class PostsController {
         List<Post> posts = DaoFactory.getPostsDao().getAllPosts();
         model.addAttribute("posts", posts);
         return "posts/index";
+    }
+    @GetMapping("/posts/create")
+    public String showCreatePage(Model model){
+        model.addAttribute("post", new Post());
+        return "posts/create";
+    }
+    @PostMapping("/posts/create")
+    public String createNewPost(@ModelAttribute Post post){
+        DaoFactory.getPostsDao().savePost(post);
+        return "redirect:/posts";
     }
 }
